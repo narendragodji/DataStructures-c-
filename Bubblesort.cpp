8a4053d9eb0d5c58f3f3f3c1771fcba2fae79695
narendragodji <82670005+narendragodji@users.noreply.github.com>
2022-08-06 18:38:21 +0530

    Create Bubblesort.cpp

---
+++ Bubblesort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int n) {
+	for(int i = 0; i < n; i++) {
+		cout << arr[i] << " " ;
+	}
+	cout << endl;
+}
+
+void bubblesort(int arr[],int n ){
+    for(int j=0;j<n-1;j++)
+    {
+        for(int i=0;i<n-1-j;i++)
+        {
+            if(arr[i]>arr[i+1])
+            {
+                int temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+            }
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    
+    int arr[100];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    bubblesort(arr,n);
+    printArray(arr,n);
+    
+    
+    
+}
