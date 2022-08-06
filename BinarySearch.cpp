ace059c4272a081b86328d0316c3e2af74fa0011
narendragodji <82670005+narendragodji@users.noreply.github.com>
2022-08-06 12:02:56 +0530

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,39 +1,38 @@
 #include <iostream>
-
 using namespace std;
-int binarysearch(int arr[],int n,int val)
-{
-    int start=0;
-    int end =n-1;
-    
-    while(start<=end){
-        int mid =(start+end)/2;
-        if(arr[mid]==val)
-        {
-            return mid;
-        }
-        else if(arr[mid]<val)
-        {
-            start=mid+1;
-        }
-        else{
-            end=mid-1;
-        }
-        
-    }
-    return -1;
+
+int binarySearch(int arr[], int n, int x) {
+	int start = 0, end = n - 1;
+	while(start <= end) {
+		int mid = (start + end) / 2;
+		if(arr[mid] == x) {
+			return mid;
+		}
+		else if(x < arr[mid]) {
+			end = mid - 1;
+		}
+		else {
+			start = mid + 1;
+		}
+	}
+
+	return -1;
 }
 
-int main()
-{
-    int n;
-    cin>>n;
-    
-    int arr[100];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    int val;
-    cin>>val;
+int main() {
+	// Take array input from the user
+	int n;
+	cin >> n;
+
+	int input[100];
+	
+	for(int i = 0; i < n; i++) {
+		cin >> input[i];
+	}
+
+	int x;
+	cin >> x;
+
+	cout << binarySearch(input, n, x) << endl; 	
 
 }
